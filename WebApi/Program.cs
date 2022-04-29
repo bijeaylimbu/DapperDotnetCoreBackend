b0024289af47700373bdeee106242726f9b9db09
bijeaylimbu <bijaylimbu11111@gmail.com>
2022-04-30 03:32:00 +0545

    removing unused files

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<TaxSlabDbContext>();
-builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IPersonRepository, TaxRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
